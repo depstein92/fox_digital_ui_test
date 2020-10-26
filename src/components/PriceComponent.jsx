d06bb1b02de6b1abee54066616d588273479fbc3
Daniel Epstein <d.epstein92@gmail.com>
2020-10-26 13:31:18 -0400

    added html for both Price Component and Profile Component

--- src/components/PriceComponent.jsx
+++ src/components/PriceComponent.jsx
@@ -2,6 +2,23 @@ import React from 'react';
 
 const PriceComponent = () => (
     <div className="price">
+        <div className="price__set-up-your-pricing-container">
+            <div className="price__set-up-your-pricing-title">
+            Set up your pricing
+            </div>
+            <div className="price__set-up-your-pricing-sub-title">
+             Please set up your hourly or fixed rates so that the 
+             client is aware of your pricing
+            </div>
+        </div>
+        <div className="price__button-container">
+            <button className="price__hourly-button">Hourly</button>
+            <button className="price__fixed-button">Fixed</button>
+        </div>
+        <div className="price__hourly-payment-slider-container">
+            <div className="hourly-payment"></div>
+            <div className="hourly-slider"></div>
+        </div>
     </div>
 );
 
--- src/components/ProfileComponent.jsx
+++ src/components/ProfileComponent.jsx
@@ -2,6 +2,28 @@ import React from 'react';
 
 const ProfileComponent = () => (
     <div className="profile">
+      <div className="profile__background-container">
+        <img src="" alt=""/>
+      </div>
+      <div className="profile__profile-user-information">
+        <div className="profile__profile-name">Filip Martin Jose</div>
+        <div className="profile__profile-location">Los Angeles</div>
+        <button>Pro Level</button>  
+      </div>
+      <div className="profile__profile-information">
+          <div className="profile__profile-followers">
+            <div className="profile__followers-title">Followers</div>
+            <div className="profile__followers">980</div>
+          </div>
+          <div className="profile__projects">
+            <div className="profile__projects-title">Projects</div>
+            <div className="profile__projects">142</div>
+          </div>
+          <div className="profile__rank">
+            <div className="profile__rank-title">Rank</div>
+            <div className="profile__rank">129</div>
+          </div>
+      </div>
     </div>
 );
 
