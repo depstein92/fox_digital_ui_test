9dfc87575bbad25bde4cbe87bc141fc22ddcf4fe
Daniel Epstein <d.epstein92@gmail.com>
2020-10-28 22:13:42 -0400

    fixed bugs for mobile

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <div className="layout-wrapper">
-        <ProfileComponent />
+      <ProfileComponent />
         <PriceComponent />
         <CheckoutComponent />
       </div>
